ddd10d9459c6feff86c0978de9d207dfe352b6bb
danielcodingal <jophyjames@gmail.com>
2024-10-17 19:28:03 +0530

    second commit

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('mydatabase.db', (err) => { if
+     (err) { console.error(err.message); } else {
+         console.log('Connected to the SQLite database.'); } });
